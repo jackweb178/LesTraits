acadf9596b982df84a2800411ddd29d53ed0b5a4
talibadio <jackweb178@gmail.com>
2020-03-27 23:16:27 +0000

    Definition d'attribut dans un trait

--- index.php
+++ index.php
@@ -1,30 +1,24 @@
 <?php
 //les traits  permettent deviter la duplicaion du code dune methode dans plusieurs classe
 
-//https://openclassrooms.com/fr/courses/1665806-programmez-en-oriente-objet-en-php/1667373-les-traits#/id/r-1670626
+//https://openclassrooms.com/fr/courses/1665806-programmez-en-oriente-objet-en-php/1667373-les-traits#/id/r-1670642
 /*
- *À l'inverse, si une classe utilise un trait possédant une méthode déjà implémentée dans la classe mère de la classe utilisant le trait, alors ce sera la méthode du trait qui sera utilisée (la méthode du trait écrasera celle de la méthode de la classe mère). Exemple :
+ *
+Nous avons vu que les traits servaient à isoler des méthodes afin de pouvoir les utiliser dans deux classes totalement indépendantes. Si le besoin s'en fait sentir, sachez que vous pouvez aussi définir des attributs dans votre trait. Ils seront alors à leur tour importés dans la classe qui utilisera ce trait. Exemple :
  * */
 trait MonTrait
 {
-    public function speak()
+    protected  $attribut = "Hello !";
+    public function showAttribut()
     {
-        echo 'je suis un trait !';
+        echo $this->attribut;
     }
 }
 
-class Mere
-{
-    public function speak()
-    {
-        echo  "je suis la classe mere !";
-    }
-}
-
-class Fille extends Mere
+class MaClasse
 {
     use MonTrait;
 }
 
-$m= new Fille();
-$m->speak();//affiche je suis un trait
+$m= new MaClasse();
+$m->showAttribut();
