4d3ea979a9127d0071c2f2fed2e45f58dea77d28
talibadio <jackweb178@gmail.com>
2020-03-27 22:39:52 +0000

    Utiliser pluysieurs traits

--- index.php
+++ index.php
@@ -1,34 +1,31 @@
 <?php
 //les traits  permettent deviter la duplicaion du code dune methode dans plusieurs classe
 
-//https://openclassrooms.com/fr/courses/1665806-programmez-en-oriente-objet-en-php/1667373-les-traits#/id/r-1670601
+//https://openclassrooms.com/fr/courses/1665806-programmez-en-oriente-objet-en-php/1667373-les-traits#/id/r-1670620
 trait HTMLFormater //declaration du trait
 {
     public function format($text)
     {
-        return '<p> Date : '.date('d/m/y').'</p>'."\n".'<p>'.nl2br($text).'</p>';
-    }
-}
-class Writer
-{
-    use HTMLFormater;
-    public function write($text)
-    {
-        file_put_contents('fichier.html' , $this->format($text));
+        return '<p> Date : '.date('d/m/y').'</p>'."\n".'<p>'.nl2br($text).'</p>';///nl2br =<br>
     }
 }
 
-class  Mailer
+trait TextFormater
 {
-    use HTMLFormater;
-    public function send($text)
+    public function formatText($text)
     {
-        mail('login@gmail.com', 'test avec les traits', $this->format($text));
+        return 'Date : '.date('yy/m/d')."\n\n".nl2br($text);
+    }
+}
+
+class Writer
+{
+    use HTMLFormater, TextFormater;
+    public function write($text)
+    {
+        file_put_contents('fichier.html' , $this->formatText($text));
     }
 }
 
 $w=new Writer;
 $w->write('hello word !');
-
-$m = new Mailer;
-$m->send('hello word !');
