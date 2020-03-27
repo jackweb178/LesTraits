77b85bfe8cbf06503d243928ef52e5a9db5a8ea1
talibadio <jackweb178@gmail.com>
2020-03-27 23:05:42 +0000

    Le trait plus fort que la classe mere ,ou trait prioritaire sur la classe mere

--- index.php
+++ index.php
@@ -1,26 +1,30 @@
 <?php
 //les traits  permettent deviter la duplicaion du code dune methode dans plusieurs classe
 
-//https://openclassrooms.com/fr/courses/1665806-programmez-en-oriente-objet-en-php/1667373-les-traits#/id/r-1670627
+//https://openclassrooms.com/fr/courses/1665806-programmez-en-oriente-objet-en-php/1667373-les-traits#/id/r-1670626
 /*
- *Si une classe déclare une méthode et qu'elle utilise un trait possédant cette même méthode, alors la méthode déclarée dans la classe l'emportera sur la méthode déclarée dans le trait. Exemple :
+ *À l'inverse, si une classe utilise un trait possédant une méthode déjà implémentée dans la classe mère de la classe utilisant le trait, alors ce sera la méthode du trait qui sera utilisée (la méthode du trait écrasera celle de la méthode de la classe mère). Exemple :
  * */
 trait MonTrait
 {
-    public function sayHello()
+    public function speak()
     {
-        echo 'Hello trait !';
+        echo 'je suis un trait !';
     }
 }
 
-class MaClasse
+class Mere
+{
+    public function speak()
+    {
+        echo  "je suis la classe mere !";
+    }
+}
+
+class Fille extends Mere
 {
     use MonTrait;
-    public function sayHello()
-    {
-        echo  "Bonjours classe";
-    }
 }
 
-$m= new MaClasse;
-$m->sayHello();
+$m= new Fille();
+$m->speak();//affiche je suis un trait
