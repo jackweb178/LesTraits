25ce0c6ba665b469e5f44eb6f76b9104ffb8048d
talibadio <jackweb178@gmail.com>
2020-03-27 22:50:58 +0000

    Resolution de conflit lorsque deux traits comporte les meme noms de methodes

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 //les traits  permettent deviter la duplicaion du code dune methode dans plusieurs classe
 
-//https://openclassrooms.com/fr/courses/1665806-programmez-en-oriente-objet-en-php/1667373-les-traits#/id/r-1670620
+//https://openclassrooms.com/fr/courses/1665806-programmez-en-oriente-objet-en-php/1667373-les-traits#/id/r-1670619
 trait HTMLFormater //declaration du trait
 {
-    public function format($text)
+    public function formatText($text)
     {
         return '<p> Date : '.date('d/m/y').'</p>'."\n".'<p>'.nl2br($text).'</p>';///nl2br =<br>
     }
@@ -20,7 +20,10 @@ trait TextFormater
 
 class Writer
 {
-    use HTMLFormater, TextFormater;
+    use HTMLFormater, TextFormater
+    {
+        HTMLFormater::formatText insteadof TextFormater;//la methode formatText() du trait HTMLFormater écrasera la méthode du même nom du trait TextFormater(si elle y est définie)
+    }
     public function write($text)
     {
         file_put_contents('fichier.html' , $this->formatText($text));
