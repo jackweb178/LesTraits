d1928f192322bf0cf3143b27bc23a65172fb4620
talibadio <jackweb178@gmail.com>
2020-03-27 23:41:05 +0000

    Methode abstraite dans les traits

--- index.php
+++ index.php
@@ -1,24 +1,15 @@
 <?php
 //les traits  permettent deviter la duplicaion du code dune methode dans plusieurs classe
 
-//https://openclassrooms.com/fr/courses/1665806-programmez-en-oriente-objet-en-php/1667373-les-traits#/id/r-1670651
+//https://openclassrooms.com/fr/courses/1665806-programmez-en-oriente-objet-en-php/1667373-les-traits#/id/r-1670656
 
+/*Enfin, sachez que l'on peut forcer la classe utilisant le trait à implémenter certaines méthodes au moyen de méthodes abstraites. Ainsi, ce code lèvera une erreur fatale :*/
 trait A
 {
-    public function traitA()
-    {
-        echo 'je suis le trais A <br>';
-    }
+    abstract public function traitA();
 }
 
-class MaClasse
+class MaClasse //on doit forcement implementer la methode traitA() dans cette classe
 {
-    use A
-    {
-        traitA as protected traitB;//renommer trait A a trait B
-    }
+    use A;
 }
-
-$m= new MaClasse();
-$m->traitA();//affiche je suis le trait A
-$m->traitB();//levera une erreur fatal car l'alias est une methode proteger
