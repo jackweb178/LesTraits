5a14d98429d005b2386a9116aea37a2a72c516ef
talibadio <jackweb178@gmail.com>
2020-03-27 23:29:43 +0000

    Changer la visibilite dune methode implementer dans un trait

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
 //les traits  permettent deviter la duplicaion du code dune methode dans plusieurs classe
 
-//https://openclassrooms.com/fr/courses/1665806-programmez-en-oriente-objet-en-php/1667373-les-traits#/id/r-1670645
-/*
- *
-Au même titre que les classes, les traits peuvent eux aussi utiliser des traits. La façon de procéder est la même qu'avec les classes, tout comme la gestion des conflits entre méthodes. Voici un exemple :
- * */
+//https://openclassrooms.com/fr/courses/1665806-programmez-en-oriente-objet-en-php/1667373-les-traits#/id/r-1670651
+
 trait A
 {
     public function traitA()
@@ -14,19 +11,13 @@ trait A
     }
 }
 
-trait B
-{
-    use A;
-    public function traitB()
-    {
-        echo '<br>je suis le trais B';
-    }
-}
 class MaClasse
 {
-    use B;
+    use A
+    {
+        traitA as protected;
+    }
 }
 
 $m= new MaClasse();
-$m->traitA();//affiche je suis le trait A
-$m->traitB();//affiche je suis le traits B
+$m->traitA();//levera une erreur car on tente d'acceder a une mehode proteger
